142afeea21106678337625b69fd01e174b1b4d2d
silvakwan1 <123783319+silvakwan1@users.noreply.github.com>
2023-10-27 00:30:57 -0300

    fds

--- src/script/script.js
+++ src/script/script.js
@@ -2,37 +2,56 @@
 import dateMaps from "./dateMap.js";
 
 const $text = document.querySelector(".text");
-const $date = document.querySelector(".date")
+const $date = document.querySelector(".date");
+
 let day;
 //This function returns the date formatted as follows: '00/00'.
 const dayAndMonth = MyDate();
-function MyDate(){
-    const now = new Date();
-    const onDay = now.getDate();
-    const month = now.getUTCMonth() +1;
-    if(month < 10){
-        return `${onDay}/0${month}`
-    }else{
-        return `${onDay}/${month}`
-    }
+function MyDate() {
+  const now = new Date();
+  const onDay = now.getDate();
+  const month = now.getUTCMonth() + 1;
+  if (month < 10) {
+    return `${onDay}/0${month}`;
+  } else {
+    return `${onDay}/${month}`;
+  }
 }
 
 // Retrieve the date and go to the object to recover the corresponding compatible value.
-day =  dateMaps()[dayAndMonth] || "erro"
+day = dateMaps()[dayAndMonth] || "erro";
 
-renderDay();
-function renderDay(){
-    return $text.textContent = `${day}/140`
-};
-
-toDate()
-function toDate(){
-    return $date.textContent = dayAndMonth
+function renderDay() {
+  $text.textContent = `${day}/140`;
 }
-console.log(toDate)
 
+toDate();
+function toDate() {
+  return ($date.textContent = dayAndMonth);
+}
 //She reloads the page every 15 seconds.
-setInterval( domReload, 15*1000);
-function domReload(){
-    return document.location.reload()
-};
+setInterval(domReload, 15 * 1000);
+function domReload() {
+  return document.location.reload();
+}
+
+function animation() {
+  renderDay();
+  const textArray = Array.from($text.textContent);
+  $text.innerHTML = "";
+
+  textArray.forEach((letra, i) => {
+    setTimeout(() => {
+      $text.innerHTML += letra;
+    }, 500 * i);
+  });
+}
+animation();
+
+(function () {
+  const ka = document.querySelector("#karol");
+  const string = "Dia de ver a karol";
+  setTimeout(() => {
+    day >= 142 ? (ka.textContent = string) : null;
+  }, 3000);
+})();
